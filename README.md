aa0bff9ec7738ecc59843c7801634ad69acb38dd
jeonghwan-kim <ej88ej@gmail.com>
2016-10-19 19:39:31 +0900

    remove button (ngClick)

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ npm i angular
 ng-app, ng-init, {{ msg }}
 
 EMCAScript 6에 대한 간단 정리
+
+bootstrap이 아니라 sementic-ui
--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@
   const app = angular.module('todomvc', []);
 
   app.controller('TodoCtrl', ($scope) => {
-    $scope.todos = [
+    let todos = $scope.todos = [
       { id: 1, title: '요가', done: false },
       { id: 2, title: '독서', done: true },
       { id: 3, title: '청소', done: false }
     ];
+
+    $scope.onRemove = todo => {
+      angular.copy(todos.filter(t => t.id !== todo.id), todos);
+    };
+
   });
 })()
