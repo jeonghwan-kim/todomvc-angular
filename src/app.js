6d27beac17a01dd4d3f062587f5b7158594b3dcb
jeonghwan-kim <ej88ej@gmail.com>
2016-10-21 18:25:56 +0900

    add no results message

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@
 
   app.controller('TodoCtrl', ($scope) => {
     let todos = $scope.todos = [
-      { id: 1, title: '요가', done: false },
-      { id: 2, title: '독서', done: true },
-      { id: 3, title: '청소', done: false }
+      { id: 1, title: 'yoga', done: false },
+      { id: 2, title: 'reading', done: true },
+      { id: 3, title: 'cleaning', done: false }
     ];
 
     $scope.onCreate = title => {
       if (title) {
         todos.push({
-          id: todos.reduce((max, t) => t.id > max.id ? t : max).id + 1,
+          id: todos.reduce((max, t) => t.id > max ? t.id : max, 0).id + 1,
           title: title,
           done: false
         });
