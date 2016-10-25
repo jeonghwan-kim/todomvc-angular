15f5d8dd991a179063b10a9ccfbc2579b3da92f5
jeonghwan-kim <ej88ej@gmail.com>
2016-10-25 23:11:12 +0900

    localStorage

--- js/services/todo-storage.service.js
+++ js/services/todo-storage.service.js
@@ -1,34 +1,23 @@
 angular.module('todomvc')
 .factory('todoStorage', () => {
   const storage = {
-    data: [{
-      id: 1,
-      title: ':private yoga',
-      done: false,
-      createdAt: Date.now()
-    }, {
-      id: 2,
-      title: ':private: reading',
-      done: true,
-      createdAt: Date.now() - 3600000 * 1
-    }, {
-      id: 3,
-      title: ':private: cleaning',
-      done: false,
-      createdAt: Date.now() - 3600000 * 2
-    }],
+    data: [],
 
     get() {
+      const t = JSON.parse(localStorage.getItem('TODO_DATA'))
+      angular.copy(t, this.data);
       return this.data;
     },
 
     create(title) {
       this.data.push({
-        id: this.todos.reduce((max, t) => t.id > max ? t.id : max, 0).id + 1,
+        id: this.data.reduce((max, t) => t.id > max ? t.id : max, 0).id + 1,
         title: title,
         done: false,
         createdAt: Date.now()
       });
+
+      localStorage.setItem('TODO_DATA', JSON.stringify(this.data));
     },
 
     destory(todo) {
